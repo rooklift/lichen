634d048340b374a9f4980c1d99fdfc0919c0f4f4
rooklift <16438795+rooklift@users.noreply.github.com>
2023-02-09 16:15:09 +0000

    colours

--- modules/draw.js
+++ modules/draw.js
@@ -2,8 +2,8 @@
 
 const types = require("./types");
 
-const team_colours = ["#ff0000ff", "#ff0000ff"];
-const factory_colours = ["#ffffffff", "#ffffffff"];
+const team_colours = ["#228be6ff", "#f03e3eff"];
+const factory_colours = ["#59a8ecff", "#f46e6eff"];
 
 function draw(replay, index, canvas, infodiv, selection) {
 
@@ -19,7 +19,7 @@ function draw(replay, index, canvas, infodiv, selection) {
 
 	let cell_size = calculate_square_size(canvas, replay.width, replay.height);
 
-	ctx.fillStyle = "#000000ff";
+	ctx.fillStyle = "#ffffffff";
 	ctx.fillRect(0, 0, cell_size * replay.width, cell_size * replay.height);
 
 	for (let x = 0; x < replay.width; x++) {
@@ -30,31 +30,27 @@ function draw(replay, index, canvas, infodiv, selection) {
 			if (replay.cell_lichen(index, x, y) > 0) {
 				ctx.fillStyle = "#33cc33ff";
 			} else if (cell_type === types.NORMAL) {
-				ctx.fillStyle = replay.cell_rubble(index, x, y) > 0 ? "#060606ff" : "#222222ff";
+				ctx.fillStyle = replay.cell_rubble(index, x, y) > 0 ? "#cdcdcdff" : "#f4f4f4ff";
 			} else if (cell_type === types.ICE) {
-				ctx.fillStyle = "#3366ccff";
+				ctx.fillStyle = "#48dbfbff";
 			} else if (cell_type === types.ORE) {
-				ctx.fillStyle = "#cc9966ff";
+				ctx.fillStyle = "#2c3e50ff";
 			}
 
-			ctx.fillRect(x * cell_size + 2, y * cell_size + 2, cell_size - 4, cell_size - 4);
+			ctx.fillRect(x * cell_size + 1, y * cell_size + 1, cell_size - 2, cell_size - 2);
 		}
 	}
 
 	for (let factory of replay.get_factories(index)) {
 		ctx.fillStyle = factory_colours[factory.team_id];
 		let [x, y] = factory.pos;
-		for (let i = x - 1; i <= x + 1; i++) {
-			for (let j = y - 1; j <= y + 1; j++) {
-				ctx.fillRect(i * cell_size + 2, j * cell_size + 2, cell_size - 4, cell_size - 4);
-			}
-		}
+		ctx.fillRect((x - 1) * cell_size + 1, (y - 1) * cell_size + 1, cell_size * 3 - 2, cell_size * 3 - 2);
 	}
 
 	for (let unit of replay.get_units(index)) {
 		ctx.fillStyle = team_colours[unit.team_id];
 		let [x, y] = unit.pos;
-		ctx.fillRect(x * cell_size + 2, y * cell_size + 2, cell_size - 4, cell_size - 4);
+		ctx.fillRect(x * cell_size + 1, y * cell_size + 1, cell_size - 2, cell_size - 2);
 	}
 
 	draw_info(replay, index, infodiv, selection);
